53fbc02c3888863cb82e48c16cbdf09563b7b49a
Wen YU <wyu861@aucklanduni.ac.nz>
2021-01-11 18:46:35 +0800

    web-homework-3

--- src/ictgradschool/web/exercise1/Dinosaur.java
+++ src/ictgradschool/web/exercise1/Dinosaur.java
@@ -5,6 +5,41 @@ import java.io.Serializable;
 public class Dinosaur implements Serializable {
 
     //TODO: Your code for 1A) here
+private String Name;
+private String Rating;
+private String Notes;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getRating() {
+        return Rating;
+    }
+
+    public void setRating(String rating) {
+        Rating = rating;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String notes) {
+        Notes = notes;
+    }
+
+
+
+
+
+
+
+
 
 }
 
--- src/ictgradschool/web/exercise1/LikesSubmission.java
+++ src/ictgradschool/web/exercise1/LikesSubmission.java
@@ -1,12 +1,27 @@
 package ictgradschool.web.exercise1;
 
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "likes-submission", urlPatterns = {"/likes-submission"})
 public class LikesSubmission extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
 
-    // TODO: your code for 1B) here
+
+
+
+
+
+
+    }
+// TODO: your code for 1B) here
 
 }
