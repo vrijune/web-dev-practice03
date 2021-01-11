ff209fa4b71f7ff7db662eeb38b388d6e7cd837b
Wen YU <wyu861@aucklanduni.ac.nz>
2021-01-11 19:41:19 +0800

    web-homework-3

--- src/ictgradschool/web/exercise1/LikesSubmission.java
+++ src/ictgradschool/web/exercise1/LikesSubmission.java
@@ -1,6 +1,9 @@
 package ictgradschool.web.exercise1;
 
 
+import com.sun.org.apache.xpath.internal.patterns.NodeTest;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,9 +16,18 @@ import java.util.List;
 public class LikesSubmission extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        Dinosaur dinosaur = new Dinosaur();
 
+        String names = req.getParameter("dinosaurName");
+        String ratings = req.getParameter("dinosaurRating");
+        String notes = req.getParameter("dinosaurNotes");
+        req.setAttribute("dinosaurName",names);
+        req.setAttribute("dinosaurRating",ratings);
+        req.setAttribute("dinosaurNotes",notes);
+        req.setAttribute("dinosaur",dinosaur);
 
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jspages/dinosaur-summary.jsp");
+        dispatcher.forward(req, resp);
 
 
 
